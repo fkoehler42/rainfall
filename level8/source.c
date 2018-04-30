593e233ea713571071df9de42ccaed38dd3fb7bc
Flavien <fkoehler@student.42.fr>
2018-04-30 11:27:12 +0200

    level8 update source

--- level8/source.c
+++ level8/source.c
@@ -4,23 +4,22 @@
 #include <sys/types.h>
 #include <stdio.h>
 
-struct auth {
-	char name[32];
-	int auth;
+struct 		auth {
+	char 	name[32];
+	int 	auth;
 };
 
 struct auth *auth;
-char *service;
+char		*service;
 
-int main(int argc, char **argv)
-{
+int 		main(int argc, char **argv) {
 	char line[128];
 
 	while(1) {
 		printf("%p, %p\n", auth, service);
 
-		if(fgets(line, sizeof(line), stdin) == NULL) break;
-		
+		if(fgets(line, sizeof(line), stdin) == NULL)
+			break;		
 		if(strncmp(line, "auth ", 5) == 0) {
 			auth = malloc(sizeof(auth));
 			memset(auth, 0, sizeof(auth));
@@ -32,12 +31,10 @@ int main(int argc, char **argv)
 		if(strncmp(line, "service", 6) == 0)
 			service = strdup(line + 7);
 		if(strncmp(line, "login", 5) == 0) {
-			if(auth->auth) {
+			if(auth->auth)
 				system("/bin/sh");
-			}
-			else {
+			else
 				printf("Password:\n");
-			}
 		}
 	}
 }
