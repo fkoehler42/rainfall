4ce71c70e292623e0c935cc99725a03b9137efe1
Flavien <fkoehler@student.42.fr>
2018-05-03 13:12:10 +0200

    bonus2 analysis WIP

---
+++ bonus2/source.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+
+void	greetuser(){
+
+}
+
+int		main(int ac, char **av) {
+	char	*lang;
+
+	strncpy(??, av[1], 40);
+	strncpy(??, av[2], 32);
+	lang = getenv("LANG");
+	if (lang != NULL) {
+
+	}
+
+	return ();
+}
